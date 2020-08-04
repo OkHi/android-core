35abc1a3e545ab40022d093e9e1d2aa46d1f788f
Julius Kiano <jgkiano@gmail.com>
2020-08-04 08:57:24 +0300

    removed auth classes

--- android-core/src/main/java/io/okhi/android_core/models/OkHiMode.java
+++
@@ -1,7 +0,0 @@
-package io.okhi.android_core.models;
-
-public class OkHiMode {
-    public static String SANDBOX = "sandbox";
-    public static String PROD = "prod";
-    public static String DEV = "dev";
-}
