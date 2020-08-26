6d203c3595aa8fd42bad32d935c993a880cb8079
Julius Kiano <jgkiano@gmail.com>
2020-08-26 16:56:52 +0300

    Use new helper methods

--- app/src/main/java/io/okhi/okhiandroidcore/MainActivity.java
+++ app/src/main/java/io/okhi/okhiandroidcore/MainActivity.java
@@ -1,16 +1,25 @@
 package io.okhi.okhiandroidcore;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
+import io.okhi.android_core.OkHi;
+import io.okhi.android_core.interfaces.OkHiRequestHandler;
 import io.okhi.android_core.models.OkHiAppContext;
 import io.okhi.android_core.models.OkHiAuth;
 import io.okhi.android_core.models.OkHiDeveloperType;
+import io.okhi.android_core.models.OkHiException;
 import io.okhi.android_core.models.OkHiPlatformType;
 
 public class MainActivity extends AppCompatActivity {
 
+    OkHi okHi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,5 +35,59 @@ public class MainActivity extends AppCompatActivity {
         CoreTest test = new CoreTest(auth);
         test.testAnonymousSignWithPhoneNumber(Secret.TEST_PHONE);
         test.testAnonymousSignWithUserId(Secret.TEST_USER_ID);
+        okHi = new OkHi(this);
+    }
+
+    class Handler implements OkHiRequestHandler<Boolean> {
+        @Override
+        public void onResult(Boolean result) {
+            startAddressVerification();
+        }
+
+        @Override
+        public void onError(OkHiException exception) {
+            showMessage(exception.getMessage());
+        }
+    }
+
+    public void startAddressVerification() {
+        boolean canStartAddressVerification = canStartAddressVerification();
+        if (canStartAddressVerification) {
+            showMessage("We are able to start address verification");
+        }
+    }
+
+    private boolean canStartAddressVerification() {
+
+        if (!OkHi.isLocationPermissionGranted(getApplicationContext())) {
+            okHi.requestLocationPermission("Hey we need location permission", "Pretty please..", new Handler());
+        } else if (!OkHi.isGooglePlayServicesAvailable(getApplicationContext())) {
+            okHi.requestEnableGooglePlayServices(new Handler());
+        } else if (!OkHi.isLocationServicesEnabled(getApplicationContext())) {
+            okHi.requestEnableLocationServices(new Handler());
+        } else {
+            return true;
+        }
+        return false;
+    }
+
+    private void showMessage(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        okHi.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        okHi.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    public void handleButtonTap(View v) {
+        startAddressVerification();
     }
 }
