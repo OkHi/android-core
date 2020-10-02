769c4b6c8a55cdf3001c5c1fe2ac6e503db2f34a
Julius Kiano <jgkiano@gmail.com>
2020-10-02 12:18:18 +0300

    added request background location

--- android-core/src/main/java/io/okhi/android_core/models/OkHiPermissionService.java
+++ android-core/src/main/java/io/okhi/android_core/models/OkHiPermissionService.java
@@ -6,8 +6,12 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.util.Log;
 
 import androidx.core.app.ActivityCompat;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import io.okhi.android_core.interfaces.OkHiRequestHandler;
 
@@ -20,20 +24,76 @@ public class OkHiPermissionService {
         this.activity = activity;
     }
 
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == Constant.LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                requestHandler.onResult(true);
+            } else {
+                requestHandler.onResult(false);
+            }
+        }
+        if (requestCode == Constant.BACKGROUND_LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (isBackgroundLocationPermissionGranted(activity)) {
+                    requestHandler.onResult(true);
+                } else {
+                    requestHandler.onResult(false);
+                }
+            } else {
+                requestHandler.onResult(false);
+            }
+        }
+    }
+
+    private ArrayList<String> getBackgroundLocationPermissions() {
+        ArrayList<String> locationPermissions = getLocationPermissions();
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
+            locationPermissions.add(Manifest.permission.ACCESS_BACKGROUND_LOCATION);
+        }
+        return locationPermissions;
+    }
+
+    private ArrayList<String> getLocationPermissions() {
+        return new ArrayList<String>(Arrays.asList(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION));
+    }
+
+    public static boolean isBackgroundLocationPermissionGranted(Context context) {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
+            int permission = ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_BACKGROUND_LOCATION);
+            return permission == PackageManager.PERMISSION_GRANTED;
+        }
+        return isLocationPermissionGranted(context);
+    }
+
     public static boolean isLocationPermissionGranted(Context context) {
         int permission = ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION);
         return permission == PackageManager.PERMISSION_GRANTED;
     }
 
-    public void requestLocationPermission(String rationaleTitle, String rationaleMessage, final OkHiRequestHandler<Boolean> requestHandler) {
+    public void requestLocationPermission(String rationaleTitle, String rationaleMessage, final OkHiRequestHandler<Boolean> handler) {
         if (isLocationPermissionGranted(activity)) {
             requestHandler.onResult(true);
             return;
         }
+        String[] permissions = new String[getLocationPermissions().size()];
+        permissions = getLocationPermissions().toArray(permissions);
+        requestPermission(permissions, Constant.LOCATION_PERMISSION_REQUEST_CODE, Manifest.permission.ACCESS_FINE_LOCATION, rationaleTitle, rationaleMessage, handler);
+    }
 
-        this.requestHandler = requestHandler;
+    public void requestBackgroundLocationPermission(String rationaleTitle, String rationaleMessage, OkHiRequestHandler<Boolean> handler) {
+        if (isBackgroundLocationPermissionGranted(activity)) {
+            requestHandler.onResult(true);
+            return;
+        }
+        String[] permissions = new String[getBackgroundLocationPermissions().size()];
+        permissions = getBackgroundLocationPermissions().toArray(permissions);
+        String rationalPermission = android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q ? Manifest.permission.ACCESS_BACKGROUND_LOCATION : Manifest.permission.ACCESS_FINE_LOCATION;
+        requestPermission(permissions, Constant.BACKGROUND_LOCATION_PERMISSION_REQUEST_CODE, rationalPermission, rationaleTitle, rationaleMessage, handler);
+    }
 
-        if (ActivityCompat.shouldShowRequestPermissionRationale(activity, Manifest.permission.ACCESS_FINE_LOCATION)) {
+    private void requestPermission(final String[] permissions, final int permissionRequestCode, String rationalPermission, String rationaleTitle, String rationaleMessage, OkHiRequestHandler<Boolean> handler) {
+        this.requestHandler = handler;
+        if (ActivityCompat.shouldShowRequestPermissionRationale(activity, rationalPermission)) {
             new AlertDialog.Builder(activity)
                     .setTitle(rationaleTitle)
                     .setMessage(rationaleMessage)
@@ -41,7 +101,7 @@ public class OkHiPermissionService {
                         @Override
                         public void onClick(DialogInterface dialogInterface, int i) {
                             //Prompt the user once explanation has been shown
-                            ActivityCompat.requestPermissions(activity, getPermissions(), Constant.LOCATION_PERMISSION_REQUEST_CODE);
+                            ActivityCompat.requestPermissions(activity, permissions, permissionRequestCode);
                         }
                     })
                     .setNegativeButton(Constant.PERMISSION_DIALOG_NEGATIVE_BUTTON_TEXT, new DialogInterface.OnClickListener() {
@@ -59,32 +119,7 @@ public class OkHiPermissionService {
                     .create()
                     .show();
         } else {
-            ActivityCompat.requestPermissions(activity, getPermissions(), Constant.LOCATION_PERMISSION_REQUEST_CODE);
+            ActivityCompat.requestPermissions(activity, permissions, permissionRequestCode);
         }
-
-    }
-
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        if (requestCode == Constant.LOCATION_PERMISSION_REQUEST_CODE) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                requestHandler.onResult(true);
-            } else {
-                requestHandler.onResult(false);
-            }
-        }
-    }
-
-    private String[] getPermissions() {
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
-            return new String[]{
-                    Manifest.permission.ACCESS_FINE_LOCATION,
-                    Manifest.permission.ACCESS_COARSE_LOCATION,
-                    Manifest.permission.ACCESS_BACKGROUND_LOCATION,
-            };
-        }
-        return new String[]{
-                Manifest.permission.ACCESS_FINE_LOCATION,
-                Manifest.permission.ACCESS_COARSE_LOCATION,
-        };
     }
 }
