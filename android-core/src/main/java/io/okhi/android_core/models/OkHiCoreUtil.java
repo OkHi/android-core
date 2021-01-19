c7662090bfce0dea13274c5a61df924d9a666456
Julius Kiano <jgkiano@gmail.com>
2021-01-19 05:59:02 +0300

    added static methods for sentry tacking

--- android-core/src/main/java/io/okhi/android_core/models/OkHiCoreUtil.java
+++ android-core/src/main/java/io/okhi/android_core/models/OkHiCoreUtil.java
@@ -1,5 +1,9 @@
 package io.okhi.android_core.models;
 
+import java.util.HashMap;
+
+import io.sentry.Sentry;
+import io.sentry.protocol.User;
 import okhttp3.Response;
 
 public class OkHiCoreUtil {
@@ -11,5 +15,41 @@ public class OkHiCoreUtil {
         } else {
             return new OkHiException(OkHiException.UNKNOWN_ERROR_CODE, OkHiException.UNKNOWN_ERROR_MESSAGE);
         }
+    }
+
+    public static void setUserException(OkHiUser user) {
+        HashMap<String, String> sentryUserDetails = new HashMap<>();
+        sentryUserDetails.put("phone", user.getPhone());
+        User sentryUser = new User();
+        sentryUser.setUsername(user.getFirstName() + " " + user.getLastName());
+        sentryUser.setId(user.getId());
+        sentryUser.setOthers(sentryUserDetails);
+        Sentry.setUser(sentryUser);
+    }
+
+    public static void setUserException(String phone) {
+        HashMap<String, String> sentryUserDetails = new HashMap<>();
+        sentryUserDetails.put("phone", phone);
+        User sentryUser = new User();
+        sentryUser.setOthers(sentryUserDetails);
+        Sentry.setUser(sentryUser);
+    }
+
+    public static void setUserException(String userId, String phone) {
+        HashMap<String, String> sentryUserDetails = new HashMap<>();
+        sentryUserDetails.put("userId", userId);
+        sentryUserDetails.put("phone", phone);
+        User sentryUser = new User();
+        sentryUser.setOthers(sentryUserDetails);
+        sentryUser.setId(userId);
+        Sentry.setUser(sentryUser);
+    }
+
+    public static void captureException(OkHiException exception) {
+        Sentry.captureException(exception);
+    }
+
+    public static void captureException(Exception exception) {
+        Sentry.captureException(exception);
     }
 }
