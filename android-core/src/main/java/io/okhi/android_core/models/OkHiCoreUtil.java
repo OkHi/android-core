b7e13f22ade631bafacee1bf5806f2dc2a290bca
Julius Kiano <jgkiano@gmail.com>
2021-01-19 13:18:22 +0300

    use phone as username added way to set env

--- android-core/src/main/java/io/okhi/android_core/models/OkHiCoreUtil.java
+++ android-core/src/main/java/io/okhi/android_core/models/OkHiCoreUtil.java
@@ -1,8 +1,13 @@
 package io.okhi.android_core.models;
 
+import android.content.Context;
+
 import java.util.HashMap;
 
 import io.sentry.Sentry;
+import io.sentry.SentryOptions;
+import io.sentry.android.core.SentryAndroid;
+import io.sentry.android.core.SentryAndroidOptions;
 import io.sentry.protocol.User;
 import okhttp3.Response;
 
@@ -20,8 +25,11 @@ public class OkHiCoreUtil {
     public static void setUserException(OkHiUser user) {
         HashMap<String, String> sentryUserDetails = new HashMap<>();
         sentryUserDetails.put("phone", user.getPhone());
+        sentryUserDetails.put("firstName", user.getFirstName());
+        sentryUserDetails.put("lastName", user.getLastName());
+        sentryUserDetails.put("userId", user.getId());
         User sentryUser = new User();
-        sentryUser.setUsername(user.getFirstName() + " " + user.getLastName());
+        sentryUser.setUsername(user.getPhone());
         sentryUser.setId(user.getId());
         sentryUser.setOthers(sentryUserDetails);
         Sentry.setUser(sentryUser);
@@ -32,6 +40,7 @@ public class OkHiCoreUtil {
         sentryUserDetails.put("phone", phone);
         User sentryUser = new User();
         sentryUser.setOthers(sentryUserDetails);
+        sentryUser.setUsername(phone);
         Sentry.setUser(sentryUser);
     }
 
@@ -42,14 +51,28 @@ public class OkHiCoreUtil {
         User sentryUser = new User();
         sentryUser.setOthers(sentryUserDetails);
         sentryUser.setId(userId);
+        sentryUser.setUsername(phone);
         Sentry.setUser(sentryUser);
     }
 
     public static void captureException(OkHiException exception) {
+        String message = exception.getMessage();
+        exception.setMessage(exception.getCode());
+        Sentry.setExtra("message", message);
         Sentry.captureException(exception);
     }
 
     public static void captureException(Exception exception) {
+        Sentry.setExtra("message", exception.getMessage());
         Sentry.captureException(exception);
+    }
+
+    public static void setEnvironment(Context context, final String env) {
+        SentryAndroid.init(context, new Sentry.OptionsConfiguration<SentryAndroidOptions>() {
+            @Override
+            public void configure(SentryAndroidOptions options) {
+                options.setEnvironment(env);
+            }
+        });
     }
 }
